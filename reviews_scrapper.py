7912b56447432f7285eb4cd82342ba971afbb60c
ducktales <abhi.gupta141@gmail.com>
2019-06-10 12:56:05 +0530

    Create reviews_scrapper.py

---
+++ reviews_scrapper.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from time import sleep
+from bs4 import BeautifulSoup, Comment
+import pandas as pd
+
+def scroll_down():
+    """A method for scrolling the page."""
+
+    # Get scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load the page.
+        sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+
+url = input("Enter the Goggle Play store url: ") 
+#driver setup
+# add path to chromedriver
+driver = webdriver.Chrome("/path/to/chromedriver")
+
+link = url + "&showAllReviews=true"
+#open the link in browser
+k = driver.get(link)
+
+scroll_down()
+
+for i in range(10000):
+    try: 
+        show_more_xpath=driver.find_element_by_xpath('//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[2]/div')
+        show_more_xpath.click()
+        scroll_down()
+    except Exception as e:
+        print ("page load complete")
+        break
+
+Ptitle = driver.find_element_by_class_name('AHFaub').text.replace(' ','')
+file_name= Ptitle+'_reviews_list.csv'
+print ("File Name: "+file_name)
+sleep(1)                    
+xpath=driver.find_element_by_xpath('//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]')
+print ("Total Review : "+ str(len(xpath.find_elements_by_xpath("//div[@jscontroller='H6eOGe']"))))
+
+reviews_df = []
+try:
+    for elem in xpath.find_elements_by_xpath("//div[@jscontroller='H6eOGe']"):
+        #print(elem)
+        content = elem.get_attribute('outerHTML')
+        #print (content)
+        soup = BeautifulSoup(content, "html.parser")
+        #print(soup.prettify())
+        date = soup.find('span',class_='p2TkOb').get_text()
+        #print(date)
+        rating_temp = soup.find('div',class_='pf5lIe')
+        rating = rating_temp.find('div',attrs={'role':'img'})['aria-label'][6:7]
+        #print(rating.find('div',attrs={'role':'img'})['aria-label'][6:7])
+        full_review=soup.find('span',attrs={'jsname':'fbQN7e'}).get_text()
+        if(len(full_review)>0):
+            review= full_review
+        else:
+            review = soup.find('span',attrs={'jsname':'bN97Pc'}).get_text()
+        temp = pd.DataFrame({'Date':date,'Rating':rating,'Review':review},index=[0])
+        print('-'*10)
+        reviews_df.append(temp)
+except Exception as e:
+    print(e)
+
+reviews_df = pd.concat(reviews_df,ignore_index=True)
+#print (reviews_df)
+reviews_df.to_csv(file_name, encoding='utf-8')
+ 
+driver.close()
